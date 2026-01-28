f3728474b1b2159e5908820352a68141739050bb
Sayed Ishita Jaha <sayedishita123@gmail.com>
2026-01-28 18:50:22 +0530

    Add switch statement to display day of the week

---
+++ task1.java
@@ -0,0 +1,24 @@
+package switch_condition;
+public class task {
+	public static void main(String[] args) {
+		int num=6;
+		switch(num) {
+		    case 1: System.out.println("today is sunday");
+		    break;
+		    case 2: System.out.println("today is monday");
+		    break;
+		    case 3: System.out.println("today is tuesday");
+		    break;
+		    case 4: System.out.println("today is wednesday");
+		    break;
+		    case 5: System.out.println("today is thursday");
+		    break;
+		    case 6: System.out.println("today is friday");
+		    break;
+		    case 7: System.out.println("today is saturday");
+		    break;
+		    default: System.out.println("invalid day");
+		}
+	}
+
+}
