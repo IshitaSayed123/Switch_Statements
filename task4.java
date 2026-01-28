d07780d27ac4611616ec762cfb696c121ef9704c
Sayed Ishita Jaha <sayedishita123@gmail.com>
2026-01-28 18:51:44 +0530

    Implement switch case for day-to-date mapping

---
+++ task4.java
@@ -0,0 +1,21 @@
+package switch_condition;
+public class tas4 {
+	public static void main(String[] args) {
+		String S="day";
+		switch(S) {
+		    case "Monday": System.out.println("Today's date is 01-jan-2026");
+		    break;
+		    case "Tuesday": System.out.println("Today's date is 02-jan-2026");
+		    break;
+		    case "Wednesday": System.out.println("Today's date is 03-jan-2026");
+		    break;
+		    case "Thursday": System.out.println("Today's date is 04-jan-2026");
+		    break;
+		    case "Friday": System.out.println("Today's date is 05-jan-2026");
+		    break;
+		    case "Saturday": System.out.println("Today's date is 06-jan-2026");
+		    break;
+		    default: System.out.println("Today's date is Invalid");
+		}
+	}
+}
